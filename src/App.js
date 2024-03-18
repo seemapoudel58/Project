20a8fd249f2536c41de1b4b90c45e7c89e00bb04
seemapoudel58 <simapoudel6@gmail.com>
2024-03-18 18:54:35 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import './index.css';
+import SectionOne from "./Components/banner/sectionOne";
+
+function App() {
+  return (
+    <SectionOne/>
+  );
+}
+
+export default App;
---
+++ src/Components/banner/sectionOne.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "./container";
+
+function SectionOne() {
+  return (
+    <Container
+      imageUrl="../images/Bphoto.png"
+      title=" Welcome to Maeum"
+      subtitle=" elegnace within  the heart of Paris"
+      buttonText={"Explore"} 
+      buttonOnClick={() => {}}/>
+  );
+}
+
+export default SectionOne;
