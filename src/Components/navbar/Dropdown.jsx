f42be8f322a1ed544b2fc14057c1677b54eedfb5
seemapoudel58 <simapoudel6@gmail.com>
2024-03-21 20:31:18 +0545

    complete navbar in mobile view

--- src/Components/navbar/Dropdown.jsx
+++ src/Components/navbar/Dropdown.jsx
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom';
 function Dropdown() {
     const[dropdown, setDropdown] = useState(false);
   return (
-    <ul className="absolute mt-2 w-48 bg-white shadow-md rounded-lg z-50" onClick={ () => setDropdown(!dropdown)}>
+    <ul className= "absolute mt-2 w-48 bg-white shadow-md rounded-lg" onClick={ () => setDropdown(!dropdown)}>
       {serviceDropdown.map((item) => (
-        <li key={item.id} className="border-b last:border-b-0 border-gray-100">
+        <li key={item.id} className="border-b last:border-b-0 border-gray-100"
+          onMouseEnter={() => setDropdown(serviceDropdown.id)}
+            onMouseLeave={() => setDropdown(null)}
+            >
           <Link
             to={item.path}
             className={`${item.cName} block px-4 py-2 text-lg text-gray-700 hover:bg-indigo-500 hover:text-white transition duration-150 ease-in-out`}
--- src/Components/navbar/nav.jsx
+++ src/Components/navbar/nav.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGem } from "react-icons/fa";
 import { NavItems } from "./Item";
 import { FaUserPlus } from "react-icons/fa";
-import Dropdown from "./Dropdown";
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 function Nav() {
-  const SignUpButton = () => (
-    <button className="flex items-center bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded transition duration-300">
-      <FaUserPlus className="mr-2" />
-      Sign Up
-    </button>
-  );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  const SignUpButton = () => {
+    return (
+      <div className="flex items-center space-x-4">
+        <button className="flex items-center bg-yellow-700 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded transition duration-300">
+          <FaUserPlus className="mr-2" />
+          Sign Up
+        </button>
+      </div>
+    );
+  };
 
   return (
     <nav className="bg-white shadow-lg ">
@@ -21,7 +36,7 @@ function Nav() {
             <div>
               <Link
                 to="/"
-                className="flex items-center space-x-2 text-indigo-600 hover:text-indigo-500"
+                className="flex items-center space-x-2 text-yellow-700"
               >
                 <FaGem className="h-8 w-8" />
                 <span className="font-semibold text-xl text-gray-800">
@@ -30,12 +45,12 @@ function Nav() {
               </Link>
             </div>
             <div className="hidden md:flex items-center justify-center">
-              <ul className="flex items-center justify-center space-x-8  font-bold text-[18px] lg:text-[21px]">
+              <ul className="flex items-center justify-center space-x-8  font-bold text-[18px] lg:text-[21px] z-10">
                 {NavItems.map((item) => (
                   <li key={item.id} className={item.cName}>
                     <Link
                       to={item.path}
-                      className="text-gray-600 hover:text-indigo-600 transition duration-300"
+                      className="text-gray-600 hover:text-yellow-600 transition duration-300"
                     >
                       {item.title}
                     </Link>
@@ -44,12 +59,50 @@ function Nav() {
               </ul>
             </div>
           </div>
-          <div className="flex items-center space-x-4">
-            <SignUpButton />
+
+          <SignUpButton />
+
+          <div className="md:hidden">
+            <IoMenu className="h-6 w-6" onClick={toggleMenu} />
           </div>
         </div>
+
+        {menuOpen && (
+          <div className="md:hidden pt-10 px-5 bg-white w-7/12 translate-x-0 fixed h-full top-0 left-0 z-10">
+            <div className="absolute top-[4%] left-[10%] translate-y-[0%]">
+              <div className=" flex justify-between items-center  ">
+                <div>
+                  <Link
+                    to="/"
+                    className="flex items-center space-x-2 text-yellow-700 hover:text-yellow-500"
+                  >
+                    <FaGem className="h-8 w-8" />
+                    <span className="font-semibold text-xl text-gray-800">
+                      MAEUM
+                    </span>
+                  </Link>
+                </div>
+
+                <div>
+                  <IoMdClose className="h-6 w-6 ml-14" onClick={closeMenu} />
+                </div>
+              </div>
+              <div className="py-10">
+                <ul className=" md:hidden ">
+                  {NavItems.map((item, index) => (
+                    <li
+                      key={index}
+                      className="text-bodyLargeRegular py-2 cursor-pointer tracking-wider uppercase"
+                    >
+                      <Link href={item.link}>{item.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+         </div>
+        )}
       </div>
-      <Dropdown />
     </nav>
   );
 }
